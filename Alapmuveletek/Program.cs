1a463a1207934b89d612cdd2a882663d7ff1456b
kalmandonath <donathk@hotmail.com>
2015-12-27 19:29:59 +0100

    Ékezetes üzenetek.

---
+++ Alapmuveletek/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            float ossz, kul, szor, hany;
+
+            Console.Write("Kérem az első számot: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Kérem a második számot: ");
+            b = int.Parse(Console.ReadLine());
+            ossz = a + b;
+            kul = Math.Abs(a - b);
+            szor = a * b;
+
+            if (a > b)
+            {
+                hany = a / b;
+            }
+            else
+            {
+                hany = b / a;
+                
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Osszeguk: " + ossz);
+            Console.WriteLine("Kulonbseguk: " + kul);
+            Console.WriteLine("Hanyadosuk: " + hany);
+            Console.WriteLine("Szorzatuk: " + szor);
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+
